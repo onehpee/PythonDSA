3700fec349bade0c722f164fe728f63184f4f885
Adedayo UWensuyi <onehpee@yahoo.com>
2023-11-29 22:19:19 -0600

    Sorting. Insertion. Recursive.

---
+++ Recursion/recursivereverse.py
@@ -0,0 +1,10 @@
+#Write a recursive function to reverse a string. 
+#The base case occurs when the string is the empty string "".
+#The recursive case involves appending the first character of 
+#the string to the reversed tail of the string, where the tail
+#is the string without the first character.
+
+def reverse(s):
+    # you write this part
+
+    print(reverse("ward"))
