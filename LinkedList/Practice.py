e982a16da94fbdd2b5f3282c7f2c7122d383eb94
Adedayo UWensuyi <onehpee@yahoo.com>
2023-12-03 22:58:27 -0600

    Linkedlist. Practice.

--- LinkedList/Practice.py
+++ LinkedList/Practice.py
@@ -15,7 +15,43 @@ class LinkedList:
         self.head = None
         for data in data_list:
             self.insert_at_end(data)
+    
+    def remove_at(self, index):
+        if index < 0 or index >= self.get_length():
+            raise Exception("Invalid index")
             
+        if index == 0:
+            self.head = self.head.next
+            return
+        
+        count = 0
+        iterate = self.head
+        while iterate:
+            if count == index - 1:
+                iterate.next = iterate.next.next
+                break
+            
+            iterate = iterate.next
+            count += 1
+    
+    def insert_at(self, index, data):
+        if index <0 or index > self.get_length():
+            raise Exception("Invalid index")
+        
+        if index == 0:
+            self.insert_at_begining(data)
+            return
+        
+        count = 0
+        iterate = self.head
+        while iterate:
+            if count == index - 1:
+                node = Node(data, iterate.next)
+                iterate.next = node
+                break
+            
+        iterate = iterate.next
+        count += 1       
             
 
 if __name__ == "__main__":
